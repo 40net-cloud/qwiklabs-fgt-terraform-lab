468f5eb5e21b490b5ff9c3bfd67dab0d7250b439
Bartek Moczulski <bmoczulski@fortinet.com>
2023-02-14 15:44:49 +0000

    re-uploaded eicar, fixed day0 import

--- labs/dayN/import-day0.tf
+++ labs/dayN/import-day0.tf
@@ -1,11 +1,8 @@
 // First, load the outputs from the day0 run so we can use them with "data.terraform_remote_state.day0.outputs"
 data "terraform_remote_state" "day0" {
-  backend = "remote"
+  backend = "local"
 
   config  = {
-    organization = "convergence22"
-    workspaces = {
-      name = "day0"
-    }
+    path = "../day0/terraform.tfstate"
   }
 }
